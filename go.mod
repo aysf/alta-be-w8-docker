fe9e8ad133dd1ce3803feab69f83f2b5e02d1eef
aywicaksono <ansufw@gmail.com>
2021-09-27 08:43:17 +0700

    add docker compose

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/aysf/alta-be-w8-docker
 go 1.16
 
 require (
+	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.6.0
 	github.com/mattn/go-colorable v0.1.10 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.0.0-20210924151903-3ad01bbaa167 // indirect
 	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
+	gorm.io/driver/mysql v1.1.2
+	gorm.io/gorm v1.21.15
 )
--- main.go
+++ main.go
@@ -1,20 +1,98 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
+type User struct {
+	Id       uint   `json:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
+
+func InitDB() {
+	godotenv.Load()
+
+	connectionString2 := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString2), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func InitMigration() {
+	DB.AutoMigrate(&User{})
+}
+
+func init() {
+	InitDB()
+	InitMigration()
+}
+
 func main() {
+
 	e := echo.New()
 
 	e.GET("/", hello)
+	e.POST("/user", createUser)
+	e.GET("/users", getUsers)
 
 	e.Start(":8080")
 
 }
 
 func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "hello world")
+	godotenv.Load()
+
+	name := os.Getenv("NAME")
+
+	greeting := fmt.Sprintf("Hello %v", name)
+
+	return c.String(http.StatusOK, greeting)
+}
+
+func createUser(c echo.Context) error {
+	newUser := new(User)
+	c.Bind(newUser)
+
+	if err := DB.Create(newUser).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    newUser,
+	})
+}
+
+func getUsers(c echo.Context) error {
+	users := new([]User)
+
+	if err := DB.Find(&users).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    users,
+	})
 }
